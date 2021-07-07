2b427ed7e12fcdb402fa3806138a8633a98515a5
TcePrepK <56453014+TcePrepK@users.noreply.github.com>
2021-07-07 16:41:05 +0300

    TSLint Fixed

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,14 @@ const app = express();
 // const data = require("./data.json");
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
+app.get("/", (req: unknown, res: { send: (arg0: string) => void }) => {
     res.send("Hello World");
 });
 
-app.get("/players", (req: any, res: { send: (arg0: any) => void }) => {
+app.get("/players", (req: unknown, res: { send: (arg0: unknown) => void }) => {
     res.send("asd");
 });
 
 app.listen(port, () => {
     console.log(`Example app is listening on port http://localhost:${port}`);
-});
-
-app.get("/", (req, res) => {
-    res.send("The sedulous hyena ate the antelope!");
 });
